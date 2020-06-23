2916c4c2d42b3a469c22311354ee0f66e8bc8966
Mishaa <mishapro@mm.st>
2020-06-23 05:20:47 +0200

    Feat : Settings up app and start homepage style

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,14 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
+import Homepage from "@/views/Homepage.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "Homepage",
+    component: Homepage
   },
   {
     path: "/about",
---
+++ src/views/Homepage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="h-full">
+    <Navbar />
+    <MainHeader class="mt-48" />
+    <!-- Todo: Center MainHeader from viewport -->
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar";
+import MainHeader from "@/components/MainHeader";
+
+export default {
+  name: "Home",
+  components: { MainHeader, Navbar }
+};
+</script>
