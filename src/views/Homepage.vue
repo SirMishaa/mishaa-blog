20e83c57fc0c2695b0529d4005bebf823f254a51
Mishaa <mishapro@mm.st>
2020-06-25 07:06:07 +0200

    Feat : Prepare Article section

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="h-full">
+  <main>
     <Navbar />
     <MainHeader class="mt-48" />
+
+    <ArticleSection />
+
     <!-- Todo: Center MainHeader from viewport -->
-  </div>
+  </main>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar";
 import MainHeader from "@/components/MainHeader";
+import ArticleSection from "@/views/ArticleSection";
 
 export default {
   name: "Home",
-  components: { MainHeader, Navbar }
+  components: { ArticleSection, MainHeader, Navbar }
 };
 </script>
