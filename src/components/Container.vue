63156fefeeb3f0e8cdfd6ff6367533788335746e
Mishaa <mishapro@mm.st>
2020-06-25 07:06:41 +0200

    Feat : Container vue component and small scss change

---
+++ src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    :class="containerClasses"
+    class="w-full sm:w-full md:w-4/5 lg:w-3/5 xl:w-3/5 mx-auto mt-32"
+  >
+    <slot></slot>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class Container extends Vue {
+  @Prop({ type: Boolean, default: false })
+  private debug;
+
+  get containerClasses(): string | null {
+    return this.$props.debug ? "border border-red-500" : null;
+  }
+}
+</script>
